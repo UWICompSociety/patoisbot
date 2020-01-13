063822e12b07cc620e812192771f00e0b5c67493
Matthew Stone <generalms16@gmail.com>
2020-01-12 19:00:00 -0500

    initial commit

---
+++ timeline_tweets.py
@@ -0,0 +1,41 @@
+import tweepy
+import argparse
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(
+	description='Arguments for authentication')
+	parser.add_argument('--consumer_key', help='consumer key', required=True)
+	parser.add_argument('--consumer_secret', help='consumer key', required=True)
+	parser.add_argument('--access_key', help='consumer key', required=True)
+	parser.add_argument('--access_secret', help='consumer key', required=True)
+
+	args = parser.parse_args()
+
+	# Authenticate to Twitter
+	auth = tweepy.OAuthHandler(args.consumer_key, args.consumer_secret)
+	auth.set_access_token(args.access_key, args.access_secret)
+
+	# Create API object
+	api = tweepy.API(auth)
+	# Create a tweet
+
+	#api.update_status("I am skynet")
+
+	#View timeline tweets
+
+	timeline = api.home_timeline()
+	for i, tweet in enumerate(timeline):
+		print(i + 1, ")", tweet.user.name, " ",  tweet.user.location)
+
+
+	#Filter tweets with query
+	for i, tweet in enumerate(api.search(q="Jamaica", lang="en", rpp=10)):
+		print(i+1, ")" , tweet.text)
+
+	user = api.get_user("realDonaldTrump")
+	print(user.name)
+	print(user.description)
+	print("Last 20 Followers:")
+	for follower in user.followers():
+		print(follower.name)
---
+++ tweets_by_location.py
@@ -0,0 +1,35 @@
+import sys
+import tweepy
+import argparse
+
+
+
+
+class CustomStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        #if 'manchester united' in status.text.lower():
+        print (status.text)
+
+    def on_error(self, status_code):
+        print('Encountered error with status code:', status_code)
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        print('Timeout...')
+        return True # Don't kill the stream
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(
+	description='Arguments for authentication')
+	parser.add_argument('--consumer_key', help='consumer key', required=True)
+	parser.add_argument('--consumer_secret', help='consumer key', required=True)
+	parser.add_argument('--access_key', help='consumer key', required=True)
+	parser.add_argument('--access_secret', help='consumer key', required=True)
+
+	args = parser.parse_args()
+
+	auth = tweepy.OAuthHandler(args.consumer_key, args.consumer_secret)
+	auth.set_access_token(args.access_key, args.access_secret)
+
+	sapi = tweepy.streaming.Stream(auth, CustomStreamListener())    
+	sapi.filter(locations=[-78.3377192858, 17.7011162379, -76.1996585761, 18.5242184514])
